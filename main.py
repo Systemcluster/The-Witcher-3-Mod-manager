968dd3a72595b041ae8ce6e1d687496485c2c14a
Systemcluster <me@systemcluster.me>
2019-05-16 19:38:16 +0200

    Elevate rights on startup

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # pylint: disable=invalid-name,missing-docstring,bare-except,broad-except,wildcard-import,unused-wildcard-import
 
 import sys
+import os
+import re
 from os import environ
 from argparse import ArgumentParser
 
@@ -23,18 +25,31 @@ if __name__ == "__main__":
         del environ["QT_DEVICE_PIXEL_RATIO"]
     environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 
-    parser = ArgumentParser(description=getVersionString())
-    parser.add_argument(
-        "-d", "--debug", dest="debug", action="store_true", default=False,
-        help="show debug information on errors")
-    parser.add_argument(
-        "-v", "--version", dest="version", action="store_true", default=False,
-        help="show version information and exit")
-    args = parser.parse_args()
-    data.debug = args.debug
-    if args.version:
-        print(getVersionString())
-        sys.exit()
+    try:
+        parser = ArgumentParser(description=getVersionString())
+        parser.add_argument(
+            "-d", "--debug", dest="debug", action="store_true", default=False,
+            help="show debug information on errors")
+        parser.add_argument(
+            "-v", "--version", dest="version", action="store_true", default=False,
+            help="show version information and exit")
+        args = parser.parse_args()
+        data.debug = args.debug
+        if args.version:
+            print(getVersionString())
+            sys.exit()
+    except Exception as e:
+        print(str(e))
+
+    # if we're a packaged executable on Windows, elevate rights and restart
+    if not re.fullmatch(r'^.*[/\\](python[0-9]*(\.exe)?)$', sys.executable, re.RegexFlag.IGNORECASE):
+        if os.name == 'nt':
+            import ctypes
+            if not ctypes.windll.shell32.IsUserAnAdmin():
+                print('Not running with admin privileges, elevating rights...')
+                ctypes.windll.shell32.ShellExecuteW(None, 'runas', sys.executable, \
+                    ' '.join(['"%s"' % (x,) for x in sys.argv[1:]]), None, 1)
+                sys.exit(0)
 
     data.app = QApplication(sys.argv)
     data.config = Configuration(getDocumentsFolder())
