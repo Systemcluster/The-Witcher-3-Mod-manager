35a810f4b873ea0e72b76f79abc28e26a65bfcec
Systemcluster <me@systemcluster.me>
2019-10-14 21:02:24 +0200

    Bump version to 0.7.10

--- src/globals/constants.py
+++ src/globals/constants.py
@@ -3,7 +3,7 @@
 
 from PyQt5.QtCore import QCoreApplication
 
-VERSION = "0.7.9"
+VERSION = "0.7.10"
 TITLE = "The Witcher 3 Mod Manager"
 AUTHORS = ["Stefan Kostic (stefan3372)", "Christian Sdunek (Systemcluster)"]
 AUTHORS_MAIL = ["stekos@live.com", "cs@systemcluster.me"]
