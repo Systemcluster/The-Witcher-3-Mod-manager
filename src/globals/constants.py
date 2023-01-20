6904d7933a5914d241cb5d5fd2ec454f96175b88
Systemcluster <me@systemcluster.me>
2023-01-20 18:58:44 +0100

    Bump version to 0.9.1

--- src/globals/constants.py
+++ src/globals/constants.py
@@ -5,9 +5,11 @@ from PySide2.QtCore import QCoreApplication
 
 translate = QCoreApplication.translate
 
-VERSION = "0.9.0"
+VERSION = "0.9.1"
 TITLE = translate("GLOBALS", "The Witcher 3 Mod Manager")
-AUTHORS = ["Stefan Kostic (stefan3372)", "Christian Sdunek (Systemcluster)", "Adam Sunderman (madman asunder)", "Henry Hsieh (henry-hsieh)"]
-AUTHORS_MAIL = ["stekos@live.com", "me@systemcluster.me", "amsunderman@gmail.com", "r901042004@yahoo.com.tw"]
+AUTHORS = ["Stefan Kostic (stefan3372)", "Christian Sdunek (Systemcluster)",
+           "Adam Sunderman (madman asunder)", "Henry Hsieh (henry-hsieh)"]
+AUTHORS_MAIL = ["stekos@live.com", "me@systemcluster.me",
+                "amsunderman@gmail.com", "r901042004@yahoo.com.tw"]
 URL_WEB = "https://www.nexusmods.com/witcher3/mods/2678"
 URL_GIT = "https://github.com/Systemcluster/The-Witcher-3-Mod-manager.git"
