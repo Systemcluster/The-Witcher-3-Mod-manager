507dca733305a087777e080d2a2650178c5dc964
Systemcluster <me@systemcluster.me>
2024-08-03 19:14:55 +0200

    Switch to rye

--- setup_.py
+++ setup_.py
@@ -46,8 +46,8 @@ setup(
     description=TITLE,
     executables=[Executable(
         "main.py",
-        targetName="TheWitcher3ModManager.exe",
+        target_name="TheWitcher3ModManager.exe",
         icon='res/w3a.ico',
-        base="Win32GUI"
+        base="gui"
     )]
 )
