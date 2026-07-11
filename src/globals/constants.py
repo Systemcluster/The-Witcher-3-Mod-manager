339426c48670e10b402c384d63e56e90ba7b1443
Systemcluster <me@systemcluster.me>
2026-07-11 22:28:52 +0200

    Bump version to 0.10.1

--- src/globals/constants.py
+++ src/globals/constants.py
@@ -5,7 +5,7 @@ from PySide6.QtCore import QCoreApplication
 
 translate = QCoreApplication.translate
 
-VERSION = "0.10.0"
+VERSION = "0.10.1"
 TITLE = translate("GLOBALS", "The Witcher 3 Mod Manager")
 AUTHORS = ["Stefan Kostic (stefan3372)", "Christian Sdunek (Systemcluster)",
            "Adam Sunderman (madman asunder)", "Henry Hsieh (henry-hsieh)", "George Burduli (burdulixda)"]
