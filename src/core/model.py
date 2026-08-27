d913c59ed71ac02b9974c21aad9b3fe1c36ace3b
Cole Fuerth <colefuerth@gmail.com>
2026-08-27 15:50:34 -0700

    missing stringio import, not imported implicitly on some interpreters

--- src/core/model.py
+++ src/core/model.py
@@ -1,6 +1,7 @@
 '''Mod management model'''
 # pylint: disable=invalid-name,missing-docstring,wildcard-import,unused-wildcard-import
 
+from io import StringIO
 from typing import Dict, List, KeysView, ValuesView
 from os import path
 import re
